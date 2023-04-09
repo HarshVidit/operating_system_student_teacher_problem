25a1bffb37f51a5fb7d842da86656469a9a334fc
HarshVidit <103620005+HarshVidit@users.noreply.github.com>
2023-04-10 02:46:24 +0530

    Update operatingsystem.cpp
    Renewed code

--- operatingsystem.cpp
+++ operatingsystem.cpp
@@ -4,13 +4,7 @@
 class student
 {
 	public:
-	int pen,paper,question_paper;
-	student(int pen,int paper,int question_paper)
-	{
-		this->pen=pen;
-		this->paper=paper;
-		this->question_paper=question_paper;
-	}	
+	int pen,paper,question_paper;	
 };
 class teacher
 {
@@ -24,252 +18,155 @@ class teacher
 		}
 		
 };
+void processing()
+{
+	
+}
 int main()
 {
-	student obj1(1,0,0),obj2(0,1,0),obj3(0,0,1);
+	student obj[3];
+	obj[1].pen=1;obj[1].paper=0;obj[1].question_paper=0;
+	obj[2].pen=0;obj[2].paper=1;obj[2].question_paper=0;
+	obj[3].pen=0;obj[3].paper=0;obj[3].question_paper=1;
 	teacher t1(10,10,10);
 	int n=1;
-	int choice;
-	printf("Enter your choice of resource allocation.\t123  321  231  132  312  213\n");
-	scanf("%d",&choice);
-	switch(choice)
+	int seq[3];
+	printf("Enter the sequence of the students for resource allocation\n");
+	for(int i=0;i<3;i++)
 	{
-		case 1:
-			while(n<2)
-			{
-			t1.paper=t1.paper-1;
-			printf("Teacher is giving paper to Student 1\n");
-			obj1.paper=obj1.paper+1;
-			printf("Teacher has now given the question paper to Studnet 1\n");
-			t1.question_paper=t1.question_paper-1;
-			obj1.question_paper=obj1.question_paper+1;
-			if(obj1.paper+obj1.pen+obj1.question_paper==3)
-			printf("Student 1 has completed his/her assignment\n\n");
-			obj1.paper=obj1.paper-1;t1.paper=t1.paper+1;
-			obj1.question_paper=obj1.question_paper-1;t1.question_paper=t1.question_paper+1;
-			
-			t1.pen=t1.pen-1;
-			printf("Teacher is giving pen to Student 2\n");
-			obj2.pen=obj2.pen+1;
-			printf("Teacher has now given the question paper to Studnet 2\n");
-			t1.question_paper=t1.question_paper-1;
-			obj2.question_paper=obj2.question_paper+1;
-			if(obj2.paper+obj2.pen+obj2.question_paper==3)
-			printf("Student 2 has completed his/her assignment\n\n");
-			obj2.pen=obj2.pen-1;t1.pen=t1.pen+1;
-			obj2.question_paper=obj2.question_paper-1;t1.question_paper=t1.question_paper+1;
-			
-			t1.pen=t1.pen-1;
-			printf("Teacher is giving pen to Student 3\n");
-			obj3.pen=obj3.pen+1;
-			printf("Teacher has now given the paper to Studnet 3\n");
-			t1.paper=t1.paper-1;
-			obj3.paper=obj3.paper+1;
-			if(obj3.paper+obj3.pen+obj3.question_paper==3)
-			printf("Student 3 has completed his/her assignment\n\n");
-			obj3.pen=obj3.pen-1;t1.pen=t1.pen+1;
-			obj3.paper=obj3.paper-1;t1.paper=t1.paper+1;
-			n++;
+		scanf("%d",&seq[i]);
+	}
+	while(n<10)
+	{
+	for(int i=0;i<3;i++)
+	{
+		if(seq[0]==1&&seq[1]==2&&seq[2]==3)
+		{
+		if(obj[seq[i]].paper==0&&obj[seq[i]].question_paper==0)
+		{t1.paper=t1.paper-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].question_paper=1;
 		}
-		break;
-		case 2:
-			while(n<10)
-			{
-			t1.pen=t1.pen-1;
-			printf("Teacher is giving pen to Student 3\n");
-			obj3.pen=obj3.pen+1;
-			printf("Teacher has now given the paper to Studnet 3\n");
-			t1.paper=t1.paper-1;
-			obj3.paper=obj3.paper+1;
-			if(obj3.paper+obj3.pen+obj3.question_paper==3)
-			printf("Student 3 has completed his/her assignment\n\n");
-			obj3.pen=obj3.pen-1;t1.pen=t1.pen+1;
-			obj3.paper=obj3.paper-1;t1.paper=t1.paper+1;
-			
-			t1.pen=t1.pen-1;
-			printf("Teacher is giving pen to Student 2\n");
-			obj2.pen=obj2.pen+1;
-			printf("Teacher has now given the question paper to Studnet 2\n");
-			t1.question_paper=t1.question_paper-1;
-			obj2.question_paper=obj2.question_paper+1;
-			if(obj2.paper+obj2.pen+obj2.question_paper==3)
-			printf("Student 2 has completed his/her assignment\n\n");
-			obj2.pen=obj2.pen-1;t1.pen=t1.pen+1;
-			obj2.question_paper=obj2.question_paper-1;t1.question_paper=t1.question_paper+1;
-			
-			
-			t1.paper=t1.paper-1;
-			printf("Teacher is giving paper to Student 1\n");
-			obj1.paper=obj1.paper+1;
-			printf("Teacher has now given the question paper to Studnet 1\n");
-			t1.question_paper=t1.question_paper-1;
-			obj1.question_paper=obj1.question_paper+1;
-			if(obj1.paper+obj1.pen+obj1.question_paper==3)
-			printf("Student 1 has completed his/her assignment\n\n");
-			obj1.paper=obj1.paper-1;t1.paper=t1.paper+1;
-			obj1.question_paper=obj1.question_paper-1;t1.question_paper=t1.question_paper+1;
-			n++;
-			
+		if(obj[seq[i]].pen==0&&obj[seq[i]].question_paper==0)
+		{
+			t1.pen=t1.pen-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].pen=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].paper==0)
+		{
+			t1.pen=t1.pen-1;t1.paper=t1.paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].pen=1;
+		}
+		if(obj[seq[i]].pen+obj[seq[i]].paper+obj[seq[i]].question_paper==3)
+		printf("Resources have been allocated to Student %d\n",seq[i]);
+		printf("Student %d has done his/her assignment\n",seq[i]);
+		t1.paper=t1.paper+2;t1.pen=t1.pen+2;t1.question_paper=t1.question_paper+2;
+	    }
+	    if(seq[0]==1&&seq[1]==3&&seq[2]==2)
+		{
+		if(obj[seq[i]].paper==0&&obj[seq[i]].question_paper==0)
+		{t1.paper=t1.paper-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].question_paper==0)
+		{
+			t1.pen=t1.pen-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].pen=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].paper==0)
+		{
+			t1.pen=t1.pen-1;t1.paper=t1.paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].pen=1;
+		}
+		if(obj[seq[i]].pen+obj[seq[i]].paper+obj[seq[i]].question_paper==3)
+		printf("Resources have been allocated to Student %d\n",seq[i]);
+		printf("Student %d has done his/her assignment\n",seq[i]);
+		t1.paper=t1.paper+2;t1.pen=t1.pen+2;t1.question_paper=t1.question_paper+2;
+	    }
+	    if(seq[0]==2&&seq[1]==1&&seq[2]==3)
+		{
+		if(obj[seq[i]].paper==0&&obj[seq[i]].question_paper==0)
+		{t1.paper=t1.paper-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].question_paper==0)
+		{
+			t1.pen=t1.pen-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].pen=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].paper==0)
+		{
+			t1.pen=t1.pen-1;t1.paper=t1.paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].pen=1;
+		}
+		if(obj[seq[i]].pen+obj[seq[i]].paper+obj[seq[i]].question_paper==3)
+		printf("Resources have been allocated to Student %d\n",seq[i]);
+		printf("Student %d has done his/her assignment\n",seq[i]);
+		t1.paper=t1.paper+2;t1.pen=t1.pen+2;t1.question_paper=t1.question_paper+2;
+	    }
+	    if(seq[0]==2&&seq[1]==3&&seq[2]==1)
+		{
+		if(obj[seq[i]].paper==0&&obj[seq[i]].question_paper==0)
+		{t1.paper=t1.paper-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].question_paper==0)
+		{
+			t1.pen=t1.pen-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].pen=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].paper==0)
+		{
+			t1.pen=t1.pen-1;t1.paper=t1.paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].pen=1;
+		}
+		if(obj[seq[i]].pen+obj[seq[i]].paper+obj[seq[i]].question_paper==3)
+		printf("Resources have been allocated to Student %d\n",seq[i]);
+		printf("Student %d has done his/her assignment\n",seq[i]);
+		t1.paper=t1.paper+2;t1.pen=t1.pen+2;t1.question_paper=t1.question_paper+2;
+	    }
+	    if(seq[0]==3&&seq[1]==1&&seq[2]==2)
+		{
+		if(obj[seq[i]].paper==0&&obj[seq[i]].question_paper==0)
+		{t1.paper=t1.paper-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].question_paper==0)
+		{
+			t1.pen=t1.pen-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].pen=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].paper==0)
+		{
+			t1.pen=t1.pen-1;t1.paper=t1.paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].pen=1;
+		}
+		if(obj[seq[i]].pen+obj[seq[i]].paper+obj[seq[i]].question_paper==3)
+		printf("Resources have been allocated to Student %d\n",seq[i]);
+		printf("Student %d has done his/her assignment\n",seq[i]);
+		t1.paper=t1.paper+2;t1.pen=t1.pen+2;t1.question_paper=t1.question_paper+2;
+	    }
+	    if(seq[0]==3&&seq[1]==2&&seq[2]==1)
+		{
+		if(obj[seq[i]].paper==0&&obj[seq[i]].question_paper==0)
+		{t1.paper=t1.paper-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].question_paper==0)
+		{
+			t1.pen=t1.pen-1;t1.question_paper=t1.question_paper-1;
+		obj[seq[i]].pen=1;obj[seq[i]].question_paper=1;
+		}
+		if(obj[seq[i]].pen==0&&obj[seq[i]].paper==0)
+		{
+			t1.pen=t1.pen-1;t1.paper=t1.paper-1;
+		obj[seq[i]].paper=1;obj[seq[i]].pen=1;
+		}
+		if(obj[seq[i]].pen+obj[seq[i]].paper+obj[seq[i]].question_paper==3)
+		printf("Resources have been allocated to Student %d\n",seq[i]);
+		printf("Student %d has done his/her assignment\n",seq[i]);
+		t1.paper=t1.paper+2;t1.pen=t1.pen+2;t1.question_paper=t1.question_paper+2;
+	    }
 	}
-	break;
-		case 3:
-			while(n<20)
-	{
-		t1.pen=t1.pen-1;
-		printf("Teacher is giving pen to Student 2\n");
-		obj2.pen=obj2.pen+1;
-		printf("Teacher has now given the question paper to Studnet 2\n");
-		t1.question_paper=t1.question_paper-1;
-		obj2.question_paper=obj2.question_paper+1;
-		if(obj2.paper+obj2.pen+obj2.question_paper==3)
-		printf("Student 2 has completed his/her assignment\n\n");
-		obj2.pen=obj2.pen-1;t1.pen=t1.pen+1;
-		obj2.question_paper=obj2.question_paper-1;t1.question_paper=t1.question_paper+1;
-		
-		t1.pen=t1.pen-1;
-		printf("Teacher is giving pen to Student 3\n");
-		obj3.pen=obj3.pen+1;
-		printf("Teacher has now given the paper to Studnet 3\n");
-		t1.paper=t1.paper-1;
-		obj3.paper=obj3.paper+1;
-		if(obj3.paper+obj3.pen+obj3.question_paper==3)
-		printf("Student 3 has completed his/her assignment\n\n");
-		obj3.pen=obj3.pen-1;t1.pen=t1.pen+1;
-		obj3.paper=obj3.paper-1;t1.paper=t1.paper+1;
-		
-		t1.paper=t1.paper-1;
-		printf("Teacher is giving paper to Student 1\n");
-		obj1.paper=obj1.paper+1;
-		printf("Teacher has now given the question paper to Studnet 1\n");
-		t1.question_paper=t1.question_paper-1;
-		obj1.question_paper=obj1.question_paper+1;
-		if(obj1.paper+obj1.pen+obj1.question_paper==3)
-		printf("Student 1 has completed his/her assignment\n\n");
-		obj1.paper=obj1.paper-1;t1.paper=t1.paper+1;
-		obj1.question_paper=obj1.question_paper-1;t1.question_paper=t1.question_paper+1;
-		n++;
-		
-	}
-	break;
-		case 4:
-			while(n<10)
-	{
-		t1.paper=t1.paper-1;
-		printf("Teacher is giving paper to Student 1\n");
-		obj1.paper=obj1.paper+1;
-		printf("Teacher has now given the question paper to Studnet 1\n");
-		t1.question_paper=t1.question_paper-1;
-		obj1.question_paper=obj1.question_paper+1;
-		if(obj1.paper+obj1.pen+obj1.question_paper==3)
-		printf("Student 1 has completed his/her assignment\n\n");
-		obj1.paper=obj1.paper-1;t1.paper=t1.paper+1;
-		obj1.question_paper=obj1.question_paper-1;t1.question_paper=t1.question_paper+1;
-		
-		t1.pen=t1.pen-1;
-		printf("Teacher is giving pen to Student 3\n");
-		obj3.pen=obj3.pen+1;
-		printf("Teacher has now given the paper to Studnet 3\n");
-		t1.paper=t1.paper-1;
-		obj3.paper=obj3.paper+1;
-		if(obj3.paper+obj3.pen+obj3.question_paper==3)
-		printf("Student 3 has completed his/her assignment\n\n");
-		obj3.pen=obj3.pen-1;t1.pen=t1.pen+1;
-		obj3.paper=obj3.paper-1;t1.paper=t1.paper+1;
-		
-		t1.pen=t1.pen-1;
-		printf("Teacher is giving pen to Student 2\n");
-		obj2.pen=obj2.pen+1;
-		printf("Teacher has now given the question paper to Studnet 2\n");
-		t1.question_paper=t1.question_paper-1;
-		obj2.question_paper=obj2.question_paper+1;
-		if(obj2.paper+obj2.pen+obj2.question_paper==3)
-		printf("Student 2 has completed his/her assignment\n\n");
-		obj2.pen=obj2.pen-1;t1.pen=t1.pen+1;
-		obj2.question_paper=obj2.question_paper-1;t1.question_paper=t1.question_paper+1;
-		n++;
-	
-	}
-		break;
-		case 5:
-			while(n<10)
-	{
-		
-		t1.pen=t1.pen-1;
-		printf("Teacher is giving pen to Student 3\n");
-		obj3.pen=obj3.pen+1;
-		printf("Teacher has now given the paper to Studnet 3\n");
-		t1.paper=t1.paper-1;
-		obj3.paper=obj3.paper+1;
-		if(obj3.paper+obj3.pen+obj3.question_paper==3)
-		printf("Student 3 has completed his/her assignment\n\n");
-		obj3.pen=obj3.pen-1;t1.pen=t1.pen+1;
-		obj3.paper=obj3.paper-1;t1.paper=t1.paper+1;
-		
-		t1.paper=t1.paper-1;
-		printf("Teacher is giving paper to Student 1\n");
-		obj1.paper=obj1.paper+1;
-		printf("Teacher has now given the question paper to Studnet 1\n");
-		t1.question_paper=t1.question_paper-1;
-		obj1.question_paper=obj1.question_paper+1;
-		if(obj1.paper+obj1.pen+obj1.question_paper==3)
-		printf("Student 1 has completed his/her assignment\n\n");
-		obj1.paper=obj1.paper-1;t1.paper=t1.paper+1;
-		obj1.question_paper=obj1.question_paper-1;t1.question_paper=t1.question_paper+1;
-		
-		t1.pen=t1.pen-1;
-		printf("Teacher is giving pen to Student 2\n");
-		obj2.pen=obj2.pen+1;
-		printf("Teacher has now given the question paper to Studnet 2\n");
-		t1.question_paper=t1.question_paper-1;
-		obj2.question_paper=obj2.question_paper+1;
-		if(obj2.paper+obj2.pen+obj2.question_paper==3)
-		printf("Student 2 has completed his/her assignment\n\n");
-		obj2.pen=obj2.pen-1;t1.pen=t1.pen+1;
-		obj2.question_paper=obj2.question_paper-1;t1.question_paper=t1.question_paper+1;
-		
-		n++;
-		
-	}
-	break;
-		case 6:
-			while(n<10)
-	{
-		t1.pen=t1.pen-1;
-		printf("Teacher is giving pen to Student 2\n");
-		obj2.pen=obj2.pen+1;
-		printf("Teacher has now given the question paper to Studnet 2\n");
-		t1.question_paper=t1.question_paper-1;
-		obj2.question_paper=obj2.question_paper+1;
-		if(obj2.paper+obj2.pen+obj2.question_paper==3)
-		printf("Student 2 has completed his/her assignment\n\n");
-		obj2.pen=obj2.pen-1;t1.pen=t1.pen+1;
-		obj2.question_paper=obj2.question_paper-1;t1.question_paper=t1.question_paper+1;
-		
-		t1.paper=t1.paper-1;
-		printf("Teacher is giving paper to Student 1\n");
-		obj1.paper=obj1.paper+1;
-		printf("Teacher has now given the question paper to Studnet 1\n");
-		t1.question_paper=t1.question_paper-1;
-		obj1.question_paper=obj1.question_paper+1;
-		if(obj1.paper+obj1.pen+obj1.question_paper==3)
-		printf("Student 1 has completed his/her assignment\n\n");
-		obj1.paper=obj1.paper-1;t1.paper=t1.paper+1;
-		obj1.question_paper=obj1.question_paper-1;t1.question_paper=t1.question_paper+1;
-		
-		t1.pen=t1.pen-1;
-		printf("Teacher is giving pen to Student 3\n");
-		obj3.pen=obj3.pen+1;
-		printf("Teacher has now given the paper to Studnet 3\n");
-		t1.paper=t1.paper-1;
-		obj3.paper=obj3.paper+1;
-		if(obj3.paper+obj3.pen+obj3.question_paper==3)
-		printf("Student 3 has completed his/her assignment\n\n");
-		obj3.pen=obj3.pen-1;t1.pen=t1.pen+1;
-		obj3.paper=obj3.paper-1;t1.paper=t1.paper+1;
-		n++;
-	
-	}
-		break;
-	}
-	return 0;
+	n++;
+}
 }
